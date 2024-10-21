7d222faf58f06bc0300a2bb48f58fadac8f09bab
Maxim <74974283+maximka76667@users.noreply.github.com>
2024-10-21 18:54:20 +0200

    fonts

--- src/App.tsx
+++ src/App.tsx
@@ -34,71 +34,85 @@ function App() {
   return (
     <div className={styles.app}>
       <div className={styles["persons"]}>
-        {persons.map(({ name, days }, index) => {
-          return (
-            <div className={styles["person"]}>
-              <input
-                className={styles["person__name-input"]}
-                type="text"
-                placeholder="Insert new person"
-                onChange={(e) => {
-                  setPersons((prev) =>
-                    prev.map((person, i) =>
-                      i === index
-                        ? {
-                            ...person,
-                            name: e.target.value,
-                          }
-                        : person
-                    )
-                  );
-                }}
-                value={name}
-              />
-              <input
-                className={styles["person__days-input"]}
-                type="number"
-                name={`days${index}`}
-                id={`days${index}`}
-                placeholder={`Days ${index}`}
-                onChange={(e) => {
-                  setPersons((prev) =>
-                    prev.map((person, i) =>
-                      i === index
-                        ? {
-                            ...person,
-                            days: parseInt(e.target.value) || (0 as number),
-                          }
-                        : person
-                    )
-                  );
-                }}
-                value={days}
-              />
-            </div>
-          );
-        })}
+        <input
+          className={styles["persons__total"]}
+          type="number"
+          name="total"
+          id="total"
+          placeholder="Total"
+          onChange={(e) =>
+            setTotal(parseFloat(e.target.value) || (0 as number))
+          }
+        />
+        <div className={styles["persons__list"]}>
+          {persons.map(({ name, days, gastos }, index) => {
+            return (
+              <div className={styles["person"]}>
+                <div className={styles["person__name"]}>
+                  <input
+                    className={styles["person__name-input"]}
+                    type="text"
+                    placeholder="Insert new person"
+                    onChange={(e) => {
+                      setPersons((prev) =>
+                        prev.map((person, i) =>
+                          i === index
+                            ? {
+                                ...person,
+                                name: e.target.value,
+                              }
+                            : person
+                        )
+                      );
+                    }}
+                    value={name}
+                  />
+                </div>
+                <div className={styles["person__info"]}>
+                  <div className={styles["person__days"]}>
+                    <input
+                      className={styles["person__days-input"]}
+                      type="text"
+                      name={`days${index}`}
+                      id={`days${index}`}
+                      placeholder={`Days ${index}`}
+                      onChange={(e) => {
+                        setPersons((prev) =>
+                          prev.map((person, i) =>
+                            i === index
+                              ? {
+                                  ...person,
+                                  days:
+                                    parseInt(e.target.value) || (0 as number),
+                                }
+                              : person
+                          )
+                        );
+                      }}
+                      value={days}
+                    />
+                    <p className={styles["person__days-title"]}>Days</p>
+                  </div>
+                  <div className={styles["person__gastos"]}>
+                    <p className={styles["person__gastos-value"]}>
+                      {(Math.ceil(gastos * 100) / 100).toFixed(2)}
+                    </p>
+                    <p className={styles["person__gastos-title"]}>Gastos</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <button
+          className={styles["persons_add-button"]}
+          onClick={() =>
+            setPersons((prev) => [...prev, { name: "", days: 30, gastos: 0 }])
+          }
+        >
+          Add
+        </button>
       </div>
-
-      <button
-        onClick={() =>
-          setPersons((prev) => [...prev, { name: "", days: 30, gastos: 0 }])
-        }
-      >
-        Add
-      </button>
-      <input
-        type="number"
-        name="total"
-        id="total"
-        placeholder="Total"
-        onChange={(e) => setTotal(parseFloat(e.target.value) || (0 as number))}
-      />
-      {persons.map(({ name, days, gastos }) => (
-        <p>
-          {name} ({days}/{daysTotal}): {gastos.toFixed(2)}
-        </p>
-      ))}
     </div>
   );
 }
