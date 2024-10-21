b8658d0024f1a04d87152be648a9cf34cdd05013
Maxim <74974283+maximka76667@users.noreply.github.com>
2024-10-21 16:56:44 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styles from "./App.module.css";
+
+interface Person {
+  name: string;
+  days: number;
+  gastos: number;
+}
 
 function App() {
+  const [total, setTotal] = useState<number>(0);
+  const [daysTotal, setDaysTotal] = useState<number>(0);
+
+  const [persons, setPersons] = useState<Person[]>([
+    { name: "Max", days: 30, gastos: 0 },
+    { name: "Olesia", days: 30, gastos: 0 },
+    { name: "Angels", days: 30, gastos: 0 },
+    { name: "Mariola", days: 30, gastos: 0 },
+  ]);
+
+  useEffect(() => {
+    setPersons((prev) =>
+      prev.map((person) => ({
+        ...person,
+        gastos: (person.days / daysTotal) * total,
+      }))
+    );
+  }, [total, daysTotal]);
+
+  useEffect(() => {
+    setDaysTotal(persons.reduce((prev, person) => prev + person.days, 0));
+  }, [persons]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <div className={styles["persons"]}>
+        {persons.map(({ name, days }, index) => {
+          return (
+            <div className={styles["person"]}>
+              <input
+                className={styles["person__name-input"]}
+                type="text"
+                placeholder="Insert new person"
+                onChange={(e) => {
+                  setPersons((prev) =>
+                    prev.map((person, i) =>
+                      i === index
+                        ? {
+                            ...person,
+                            name: e.target.value,
+                          }
+                        : person
+                    )
+                  );
+                }}
+                value={name}
+              />
+              <input
+                className={styles["person__days-input"]}
+                type="number"
+                name={`days${index}`}
+                id={`days${index}`}
+                placeholder={`Days ${index}`}
+                onChange={(e) => {
+                  setPersons((prev) =>
+                    prev.map((person, i) =>
+                      i === index
+                        ? {
+                            ...person,
+                            days: parseInt(e.target.value) || (0 as number),
+                          }
+                        : person
+                    )
+                  );
+                }}
+                value={days}
+              />
+            </div>
+          );
+        })}
+      </div>
+
+      <button
+        onClick={() =>
+          setPersons((prev) => [...prev, { name: "", days: 30, gastos: 0 }])
+        }
+      >
+        Add
+      </button>
+      <input
+        type="number"
+        name="total"
+        id="total"
+        placeholder="Total"
+        onChange={(e) => setTotal(parseFloat(e.target.value) || (0 as number))}
+      />
+      {persons.map(({ name, days, gastos }) => (
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {name} ({days}/{daysTotal}): {gastos.toFixed(2)}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
   );
 }
